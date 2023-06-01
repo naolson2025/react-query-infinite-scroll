efcaecf8ec23d86c05f8ca20b7511d48ff3a4ede
Nick Olson <naolson.1819@gmail.com>
2023-06-01 07:48:53 -0500

    set up react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import "./App.css";
 import { InfinitePeople } from "./people/InfinitePeople";
 import { InfiniteSpecies } from "./species/InfiniteSpecies";
+// import the react-query provider
+import { QueryClient, QueryClientProvider } from "react-query";
+// import the react-query devtools
+import { ReactQueryDevtools } from "react-query/devtools";
+
+// create a client
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <h1>Infinite SWAPI</h1>
-      <InfinitePeople />
-      {/* <InfiniteSpecies /> */}
-    </div>
+    // wrap the app in the provider
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Infinite SWAPI</h1>
+        <InfinitePeople />
+        {/* <InfiniteSpecies /> */}
+        {/* add the devtools */}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </div>
+    </QueryClientProvider>
   );
 }
 
